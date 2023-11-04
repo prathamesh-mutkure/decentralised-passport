f83bfb2278b9f5d69a762fbefbdd9772b1a19512
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-11-04 21:29:51 +0530

    Added Dadhboard Main Page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { format } from "date-fns";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date?: Date) {
+  if (!date) return "";
+  return format(date, "dd/MM/yyyy");
 }
--- src/pages/dashboard/user/index.tsx
+++ src/pages/dashboard/user/index.tsx
@@ -7,8 +7,39 @@ import { Icons } from "@/components/icons";
 import Link from "next/link";
 import Head from "next/head";
 import DashboardLayout from "~/components/layout/dashboard-layout";
+import { api } from "~/utils/api";
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
+import { formatDate } from "~/lib/utils";
+import { useUser } from "@clerk/nextjs";
 
 export default function UserDashboardPage() {
+  const { user } = useUser();
+
+  const { data: userData, isLoading: userDataLoading } =
+    api.user.getUserData.useQuery();
+
+  function DataItem({
+    label,
+    value,
+  }: {
+    label: string;
+    value?: string | null;
+  }) {
+    return (
+      <div>
+        <p className="text-sm text-muted-foreground">{label}</p>
+        <p className="text-md font-bold capitalize">{value ?? "-"}</p>
+      </div>
+    );
+  }
+
+  const getFileNo = () =>
+    `<<<<${userData?.userData.country}<<<<${userData?.userData.name
+      .split(" ")
+      .join("<<<")}<<<<<<<<<<<${userData?.userData.id}<<<<<<<${userData
+      ?.userData.nationalIdType}<<<${userData?.userData
+      .nationalId}<<<<<<<<${userData?.userData.email}<<<<<<<`.toUpperCase();
+
   return (
     <>
       <Head>
@@ -32,6 +63,31 @@ export default function UserDashboardPage() {
             </Link>
           </DashboardHeader>
         </DashboardShell>
+
+        <Card className="pt-6">
+          <CardContent>
+            <div className="grid grid-cols-3 gap-8">
+              <div>
+                {user?.hasImage ? (
+                  <img src={user?.imageUrl} className="rounded-lg" />
+                ) : (
+                  <div className="h-100 w-100 rounded-lg bg-slate-700 p-6"></div>
+                )}
+              </div>
+              <div className="col-span-2 flex flex-col gap-y-4">
+                <DataItem label="Name" value={userData?.userData.name} />
+                <DataItem
+                  label="Date of Birth"
+                  value={formatDate(userData?.userData.dob)}
+                />
+                <DataItem label="City" value={userData?.userData.city} />
+                <DataItem label="State" value={userData?.userData.region} />
+                <DataItem label="Country" value={userData?.userData.country} />
+              </div>
+              <p className="w-100 col-span-3 break-words">{getFileNo()}</p>
+            </div>
+          </CardContent>
+        </Card>
       </DashboardLayout>
     </>
   );
